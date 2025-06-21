66f890d739448f0688d4089ff3cab9287a0fd6ee
Cory Roush <coryroush@gmail.com>
2025-06-21 10:20:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 console.log("app.js loaded successfully!");
-
-// Check if adversaries loaded
+console.log("About to check adversaryDatabase...");
 console.log("adversaryDatabase exists:", typeof adversaryDatabase !== 'undefined');
-
-// Basic variables
-let encounter = [];
-let totalBattlePoints = 14;
-
-console.log("Variables initialized");
+console.log("This line should appear if the above worked");
